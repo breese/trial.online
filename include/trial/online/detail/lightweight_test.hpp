3bad9db4eb636a6548becda9db8400f72d6e5055
Bjorn Reese <(none)>
2016-12-30 16:52:06 +0100

    Added TRIAL_ONLINE_TEST_ALL_WITH to check containers for mismatches using a predicate

--- include/trial/online/detail/lightweight_test.hpp
+++ include/trial/online/detail/lightweight_test.hpp
@@ -12,6 +12,8 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <cmath>
+#include <algorithm>
+#include <sstream>
 #include <boost/detail/lightweight_test.hpp>
 
 namespace trial
@@ -41,6 +43,39 @@ inline void test_close_impl(char const * expr1,
     }
 }
 
+template<class InputIt1, class InputIt2, class Predicate>
+inline void test_mismatch_impl(char const * file, int line, char const * function,
+                               InputIt1 first_begin, InputIt1 first_end,
+                               InputIt2 second_begin, InputIt2 second_end,
+                               Predicate predicate)
+{
+    if (std::distance(first_begin, first_end) != std::distance(second_begin, second_end))
+    {
+        ::boost::detail::error_impl("Container sizes are different", __FILE__, __LINE__, BOOST_CURRENT_FUNCTION);
+    }
+    else
+    {
+        std::pair<InputIt1, InputIt2> result = std::mismatch(first_begin, first_end, second_begin, predicate);
+        if (result.first == first_end)
+        {
+            boost::detail::report_errors_remind();
+        }
+        else
+        {
+            std::ostringstream indices;
+            while (result.first != first_end)
+            {
+                indices << ' ' << std::distance(first_begin, result.first);
+                result = std::mismatch(++result.first, first_end, ++result.second);
+            }
+            BOOST_LIGHTWEIGHT_TEST_OSTREAM
+                << file << "(" << line << "): Container contents differ in function '" << function << "': mismatching indices"
+                << indices.str() << std::endl;
+            ++boost::detail::test_errors();
+        }
+    }
+}
+
 } // namespace detail
 } // namespace online
 } // namespace trial
@@ -49,5 +84,7 @@ inline void test_close_impl(char const * expr1,
 #define TRIAL_ONLINE_TEST_EQUAL BOOST_TEST_EQ
 
 #define TRIAL_ONLINE_TEST_CLOSE(LHS, RHS, TOLERANCE) ::trial::online::detail::test_close_impl(#LHS, #RHS, __FILE__, __LINE__, BOOST_CURRENT_FUNCTION, LHS, RHS, TOLERANCE)
+
+#define TRIAL_ONLINE_TEST_ALL_WITH(FIRST_BEGIN, FIRST_END, SECOND_BEGIN, SECOND_END, PREDICATE) ::trial::online::detail::test_mismatch_impl(__FILE__, __LINE__, BOOST_CURRENT_FUNCTION, FIRST_BEGIN, FIRST_END, SECOND_BEGIN, SECOND_END, PREDICATE)
 
 #endif // TRIAL_ONLINE_DETAIL_LIGHTWEIGHT_TEST_HPP
