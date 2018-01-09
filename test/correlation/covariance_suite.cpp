3e25ceb1d2733657b2094ccee0d43f53df915525
Bjorn Reese <breese@users.sourceforge.net>
2018-01-09 23:40:45 +0100

    Added test suite for covariance properties

--- test/correlation/covariance_suite.cpp
+++ test/correlation/covariance_suite.cpp
@@ -9,6 +9,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <trial/online/detail/lightweight_test.hpp>
+#include <trial/online/average/cumulative_variance.hpp>
 #include <trial/online/correlation/covariance.hpp>
 
 //-----------------------------------------------------------------------------
@@ -135,7 +136,7 @@ void test_down_up_by_one()
     TRIAL_ONLINE_TEST_CLOSE(filter.value(), -2.0, tolerance);
 }
 
-void test()
+void run()
 {
     test_empty();
     test_clear();
@@ -151,12 +152,208 @@ void test()
 } // namespace double_suite
 
 //-----------------------------------------------------------------------------
+
+namespace properties_suite
+{
+
+void test_constant()
+{
+    // Cov(X, a) = 0
+    const double a = 0.0;
+    trial::online::correlation::covariance<double> filter;
+    filter.push(0.0, a);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 0.0);
+    filter.push(1.0, a);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 0.0);
+    filter.push(10.0, a);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 0.0);
+    filter.push(100.0, a);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 0.0);
+}
+
+void test_same()
+{
+    // Cov(X, X) = Var(X)
+    trial::online::correlation::covariance<double> filter;
+    trial::online::average::cumulative_variance<double> average;
+    {
+        double x = 0.0;
+        filter.push(x, x);
+        average.push(x);
+        TRIAL_ONLINE_TEST_EQUAL(filter.value(), average.variance());
+    }
+    {
+        double x = 1.0;
+        filter.push(x, x);
+        average.push(x);
+        TRIAL_ONLINE_TEST_EQUAL(filter.value(), average.variance());
+    }
+    {
+        double x = 10.0;
+        filter.push(x, x);
+        average.push(x);
+        TRIAL_ONLINE_TEST_EQUAL(filter.value(), average.variance());
+    }
+    {
+        double x = 100.0;
+        filter.push(x, x);
+        average.push(x);
+        TRIAL_ONLINE_TEST_EQUAL(filter.value(), average.variance());
+    }
+}
+
+void test_commutative()
+{
+    // Cov(X, Y) = Cov(Y, X)
+    const double tolerance = 1e-6;
+    trial::online::correlation::covariance<double> lhs;
+    trial::online::correlation::covariance<double> rhs;
+    {
+        const double x = 0.0;
+        const double y = 0.0;
+        lhs.push(x, y);
+        rhs.push(y, x);
+        TRIAL_ONLINE_TEST_CLOSE(lhs.value(), rhs.value(), tolerance);
+    }
+    {
+        const double x = -1.0;
+        const double y = 1.0;
+        lhs.push(x, y);
+        rhs.push(y, x);
+        TRIAL_ONLINE_TEST_CLOSE(lhs.value(), rhs.value(), tolerance);
+    }
+    {
+        const double x = -2.0;
+        const double y = 10.0;
+        lhs.push(x, y);
+        rhs.push(y, x);
+        TRIAL_ONLINE_TEST_CLOSE(lhs.value(), rhs.value(), tolerance);
+    }
+    {
+        const double x = -3.0;
+        const double y = 100.0;
+        lhs.push(x, y);
+        rhs.push(y, x);
+        TRIAL_ONLINE_TEST_CLOSE(lhs.value(), rhs.value(), tolerance);
+    }
+    {
+        const double x = -4.0;
+        const double y = 1000.0;
+        lhs.push(x, y);
+        rhs.push(y, x);
+        TRIAL_ONLINE_TEST_CLOSE(lhs.value(), rhs.value(), tolerance);
+    }
+}
+
+void test_distributive()
+{
+    // Cov(aX, bY) = ab Cov(Y, X)
+    const double tolerance = 1e-6;
+    trial::online::correlation::covariance<double> lhs;
+    trial::online::correlation::covariance<double> rhs;
+    const double a = 0.5;
+    const double b = 2.0;
+    {
+        const double x = 0.0;
+        const double y = 0.0;
+        lhs.push(a * x, b * y);
+        rhs.push(x, y);
+        TRIAL_ONLINE_TEST_CLOSE(lhs.value(), a * b * rhs.value(), tolerance);
+    }
+    {
+        const double x = -1.0;
+        const double y = 1.0;
+        lhs.push(a * x, b * y);
+        rhs.push(x, y);
+        TRIAL_ONLINE_TEST_CLOSE(lhs.value(), a * b * rhs.value(), tolerance);
+    }
+    {
+        const double x = -2.0;
+        const double y = 10.0;
+        lhs.push(a * x, b * y);
+        rhs.push(x, y);
+        TRIAL_ONLINE_TEST_CLOSE(lhs.value(), a * b * rhs.value(), tolerance);
+    }
+    {
+        const double x = -3.0;
+        const double y = 100.0;
+        lhs.push(a * x, b * y);
+        rhs.push(x, y);
+        TRIAL_ONLINE_TEST_CLOSE(lhs.value(), a * b * rhs.value(), tolerance);
+    }
+    {
+        const double x = -4.0;
+        const double y = 1000.0;
+        lhs.push(a * x, b * y);
+        rhs.push(x, y);
+        TRIAL_ONLINE_TEST_CLOSE(lhs.value(), a * b * rhs.value(), tolerance);
+    }
+}
+
+void test_shift_invariant()
+{
+    // Cov(X + a, Y + b) = Cov(X, Y)
+    const double tolerance = 1e-6;
+    trial::online::correlation::covariance<double> lhs;
+    trial::online::correlation::covariance<double> rhs;
+    const double a = 0.5;
+    const double b = 2.0;
+    {
+        const double x = 0.0;
+        const double y = 0.0;
+        lhs.push(x + a, y + b);
+        rhs.push(x, y);
+        TRIAL_ONLINE_TEST_CLOSE(lhs.value(), rhs.value(), tolerance);
+    }
+    {
+        const double x = -1.0;
+        const double y = 1.0;
+        lhs.push(x + a, y + b);
+        rhs.push(x, y);
+        TRIAL_ONLINE_TEST_CLOSE(lhs.value(), rhs.value(), tolerance);
+    }
+    {
+        const double x = -2.0;
+        const double y = 10.0;
+        lhs.push(x + a, y + b);
+        rhs.push(x, y);
+        TRIAL_ONLINE_TEST_CLOSE(lhs.value(), rhs.value(), tolerance);
+    }
+    {
+        const double x = -3.0;
+        const double y = 100.0;
+        lhs.push(x + a, y + b);
+        rhs.push(x, y);
+        TRIAL_ONLINE_TEST_CLOSE(lhs.value(), rhs.value(), tolerance);
+    }
+    {
+        const double x = -4.0;
+        const double y = 1000.0;
+        lhs.push(x + a, y + b);
+        rhs.push(x, y);
+        TRIAL_ONLINE_TEST_CLOSE(lhs.value(), rhs.value(), tolerance);
+    }
+}
+
+void run()
+{
+    test_constant();
+    test_same();
+    test_commutative();
+    test_distributive();
+    test_shift_invariant();
+}
+
+} // namespace properties_suite
+
+//-----------------------------------------------------------------------------
 // main
 //-----------------------------------------------------------------------------
 
 int main()
 {
-    double_suite::test();
+    double_suite::run();
+    properties_suite::run();
 
     return boost::report_errors();
 }
