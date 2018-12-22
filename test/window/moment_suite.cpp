946a46254c49da37fd25a5b754845854d114ccd6
Bjorn Reese <breese@users.sourceforge.net>
2018-12-22 12:13:24 +0100

    Added window::moment test suite

--- test/window/moment_suite.cpp
+++ test/window/moment_suite.cpp
@@ -18,7 +18,7 @@ using namespace trial::online;
 namespace average_double_1_suite
 {
 
-void test_empty()
+void test_ctor()
 {
     window::moment<double, 1> filter;
     TRIAL_ONLINE_TEST_EQUAL(filter.capacity(), 1);
@@ -51,7 +51,7 @@ void test_many()
 
 void run()
 {
-    test_empty();
+    test_ctor();
     test_many();
 }
 
@@ -62,7 +62,7 @@ void run()
 namespace average_double_2_suite
 {
 
-void test_empty()
+void test_ctor()
 {
     window::moment<double, 2> filter;
     TRIAL_ONLINE_TEST_EQUAL(filter.capacity(), 2);
@@ -132,7 +132,7 @@ void test_clear()
 
 void run()
 {
-    test_empty();
+    test_ctor();
     test_same();
     test_one();
     test_two();
@@ -144,10 +144,109 @@ void run()
 
 //-----------------------------------------------------------------------------
 
+namespace average_double_4_suite
+{
+
+void test_ctor()
+{
+    window::moment<double, 4> filter;
+    TRIAL_ONLINE_TEST_EQUAL(filter.capacity(), 4);
+    TRIAL_ONLINE_TEST_EQUAL(filter.empty(), true);
+    TRIAL_ONLINE_TEST_EQUAL(filter.full(), false);
+    TRIAL_ONLINE_TEST_EQUAL(filter.size(), 0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 0.0);
+}
+
+void test_same()
+{
+    window::moment<double, 4> filter;
+    filter.push(1.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.size(), 1);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 1.0);
+    filter.push(1.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.size(), 2);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 1.0);
+    filter.push(1.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.size(), 3);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 1.0);
+    filter.push(1.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.size(), 4);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 1.0);
+    filter.push(1.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.empty(), false);
+    TRIAL_ONLINE_TEST_EQUAL(filter.full(), true);
+    TRIAL_ONLINE_TEST_EQUAL(filter.size(), 4);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 1.0);
+}
+
+void test_linear_increase()
+{
+    window::moment<double, 4> filter;
+    filter.push(1.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 1.0);
+    filter.push(2.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 1.5);
+    filter.push(3.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 2.0);
+    filter.push(4.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 2.5);
+    filter.push(5.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 3.5);
+    filter.push(6.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 4.5);
+}
+
+void test_linear_increase_from_offset()
+{
+    window::moment<double, 4> filter;
+    filter.push(10.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 10.0);
+    filter.push(11.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 10.5);
+    filter.push(12.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 11.0);
+    filter.push(13.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 11.5);
+    filter.push(14.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 12.5);
+    filter.push(15.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 13.5);
+}
+
+void test_linear_decrease()
+{
+    window::moment<double, 4> filter;
+    filter.push(10.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 10.0);
+    filter.push(9.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 9.5);
+    filter.push(8.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 9.0);
+    filter.push(7.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 8.5);
+    filter.push(6.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 7.5);
+    filter.push(5.0);
+    TRIAL_ONLINE_TEST_EQUAL(filter.value(), 6.5);
+}
+
+void run()
+{
+    test_ctor();
+    test_same();
+    test_linear_increase();
+    test_linear_increase_from_offset();
+    test_linear_decrease();
+}
+
+} // namespace average_double_4_suite
+
+//-----------------------------------------------------------------------------
+
 namespace average_float_2_suite
 {
 
-void test_empty()
+void test_ctor()
 {
     window::moment<float, 2> filter;
     TRIAL_ONLINE_TEST_EQUAL(filter.capacity(), 2);
@@ -217,7 +316,7 @@ void test_clear()
 
 void run()
 {
-    test_empty();
+    test_ctor();
     test_same();
     test_one();
     test_two();
@@ -232,7 +331,7 @@ void run()
 namespace average_int_2_suite
 {
 
-void test_empty()
+void test_ctor()
 {
     window::moment<int, 2> filter;
     TRIAL_ONLINE_TEST_EQUAL(filter.capacity(), 2);
@@ -302,7 +401,7 @@ void test_clear()
 
 void run()
 {
-    test_empty();
+    test_ctor();
     test_same();
     test_one();
     test_two();
@@ -317,7 +416,7 @@ void run()
 namespace variance_double_1_suite
 {
 
-void test_empty()
+void test_ctor()
 {
     window::moment_variance<double, 1> filter;
     TRIAL_ONLINE_TEST_EQUAL(filter.capacity(), 1);
@@ -358,7 +457,7 @@ void test_many()
 
 void run()
 {
-    test_empty();
+    test_ctor();
     test_many();
 }
 
@@ -369,7 +468,7 @@ void run()
 namespace variance_double_2_suite
 {
 
-void test_empty()
+void test_ctor()
 {
     window::moment_variance<double, 2> filter;
     TRIAL_ONLINE_TEST_EQUAL(filter.capacity(), 2);
@@ -458,7 +557,7 @@ void test_clear()
 
 void run()
 {
-    test_empty();
+    test_ctor();
     test_same();
     test_one();
     test_two();
@@ -476,6 +575,7 @@ int main()
 {
     average_double_1_suite::run();
     average_double_2_suite::run();
+    average_double_4_suite::run();
     average_float_2_suite::run();
     average_int_2_suite::run();
 
