084db038734d93ed24fc7b2ab351b7728a3816de
Bjorn Reese <breese@users.sourceforge.net>
2019-05-28 13:20:04 +0200

    Added interim::psquare_quantile test cases with decreasing sequences

--- test/interim/psquare_quantile_suite.cpp
+++ test/interim/psquare_quantile_suite.cpp
@@ -215,10 +215,172 @@ void test_linear_increase_with_approximation()
     TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), 13.0, tolerance); // 14.0
 }
 
+void test_linear_decrease()
+{
+    const double tolerance = 1e-4;
+    interim::basic_psquare_quantile<double, 12, std::ratio<1,2>> filter;
+
+    filter.push(100);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), 100.0, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), 100.0, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), 100.0, tolerance);
+
+    filter.push(99);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), 99.0, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), 99.5, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), 100.0, tolerance);
+
+    filter.push(98);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), 98.0, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), 99.0, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), 100.0, tolerance);
+
+    filter.push(97);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), 97.0, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), 98.5, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), 100.0, tolerance);
+
+    filter.push(96);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), 96.0, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), 98.0, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), 100.0, tolerance);
+
+    // Switch to P^2 approximation
+
+    filter.push(95);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), 95.0, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), 97.0, tolerance); // 97.5
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), 100.0, tolerance);
+
+    // Switch to interpolation
+
+    filter.push(94);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), 94.7143, tolerance); // 94.0
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), 96.1429, tolerance); // 96.5
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), 98.2857, tolerance); // 99.0
+
+    filter.push(93);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), 94.0, tolerance); // 93.0
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), 95.25, tolerance); // 95.5
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), 97.0, tolerance); // 98.0
+
+    filter.push(92);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), 93.0, tolerance); // 92.0
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), 94.3333, tolerance); // 94.5
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), 96.0, tolerance); // 97.0
+
+    filter.push(91);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), 91.8, tolerance); // 91.0
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), 93.4, tolerance); // 93.5
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), 95.2, tolerance); // 96.0
+
+    filter.push(90);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), 90.4545, tolerance); // 90.0
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), 92.4545, tolerance); // 92.5
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), 94.5455, tolerance); // 95.0
+
+    filter.push(89);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), 89.0, tolerance); // 89.0
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), 91.0, tolerance); // 91.5
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), 94.0, tolerance); // 94.0
+
+    filter.push(88);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), 88.7143, tolerance); // 88.0
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), 90.1429, tolerance); // 90.5
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), 92.2857, tolerance); // 93.0
+
+    filter.push(87);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), 88.0, tolerance); // 87.0
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), 89.25, tolerance); // 89.5
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), 91.0, tolerance); // 92.0
+}
+
+void test_linear_decrease_negative()
+{
+    const double tolerance = 1e-4;
+    interim::basic_psquare_quantile<double, 12, std::ratio<1,2>> filter;
+
+    filter.push(-1);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), -1.0, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), -1.0, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), -1.0, tolerance);
+
+    filter.push(-2);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), -2.0, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), -1.5, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), -1.0, tolerance);
+
+    filter.push(-3);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), -3.0, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), -2.0, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), -1.0, tolerance);
+
+    filter.push(-4);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), -4.0, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), -2.5, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), -1.0, tolerance);
+
+    filter.push(-5);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), -5.0, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), -3.0, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), -1.0, tolerance);
+
+    // Switch to P^2 approximation
+
+    filter.push(-6);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), -6.0, tolerance);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), -4.0, tolerance); // -3.5
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), -1.0, tolerance);
+
+    // Switch to interpolation
+
+    filter.push(-7);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), -6.28571, tolerance); // -7.0
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), -4.85714, tolerance); // -4.5
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), -2.71429, tolerance); // -2.0
+
+    filter.push(-8);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), -7.0, tolerance); // -8.0
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), -5.75, tolerance); // -5.5
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), -4.0, tolerance); // -3.0
+
+    filter.push(-9);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), -8.0, tolerance); // -9.0
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), -6.66667, tolerance); // -6.5
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), -5.0, tolerance); // -4.0
+
+    filter.push(-10);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), -9.2, tolerance); // -10.0
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), -7.6, tolerance); // -7.5
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), -5.8, tolerance); // -5.0
+
+    filter.push(-11);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), -10.5455, tolerance); // -11.0
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), -8.54545, tolerance); // -8.5
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), -6.45455, tolerance); // -6.0
+
+    filter.push(-12);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), -12.0, tolerance); // -12.0
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), -10.0, tolerance); // -9.5
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), -7.0, tolerance); // -7.0
+
+    filter.push(-13);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), -12.2857, tolerance); // -13.0
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), -10.8571, tolerance); // -10.5
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), -8.71429, tolerance); // -8.0
+
+    filter.push(-14);
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<0>(), -13.0, tolerance); // -14.0
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<1>(), -11.75, tolerance); // -11.5
+    TRIAL_ONLINE_TEST_CLOSE(filter.get<2>(), -10.0, tolerance); // -9.0
+}
+
 void run()
 {
     test_linear_increase();
     test_linear_increase_with_approximation();
+    test_linear_decrease();
+    test_linear_decrease_negative();
 }
 
 } // namespace double_quartile_suite
